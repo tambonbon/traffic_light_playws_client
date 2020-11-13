2af5efe86c205a5736efcbf53854a221265a5abf
tambonbon <tam_nguyenthanh@outlook.com>
2020-11-13 17:10:42 +0100

    ini

--- src/main/scala/models/Cars.scala
+++ src/main/scala/models/Cars.scala
@@ -14,6 +14,7 @@ object Cars {
   import DefaultBodyReadables._
 
   import scala.concurrent.ExecutionContext.Implicits._
+
   val host = "http://localhost:9000"
 
   def main(args: Array[String]): Unit = {
@@ -30,17 +31,17 @@ object Cars {
     // "AhcWSClientConfigFactory.forConfig(ConfigFactory.load, this.getClass.getClassLoader)"
     val wsClient = StandaloneAhcWSClient()
 
-//    call(wsClient)
-//      .andThen { case _ => wsClient.close() }
-//      .andThen { case _ => system.terminate() }
+    //    call(wsClient)
+    //      .andThen { case _ => wsClient.close() }
+    //      .andThen { case _ => system.terminate() }
 
     callToRed(wsClient)
+    callToRedWithPath(wsClient)
       .andThen { case _ => wsClient.close() }
       .andThen { case _ => system.terminate() }
-
-//    goToRed(wsClient)
-//      .andThen { case _ => wsClient.close() }
-//      .andThen { case _ => system.terminate() }
+    //    goToRed(wsClient)
+    //      .andThen { case _ => wsClient.close() }
+    //      .andThen { case _ => system.terminate() }
   }
 
   def call(wsClient: StandaloneWSClient): Future[Unit] = {
@@ -51,40 +52,48 @@ object Cars {
       println(s"Got a response $statusText: $body")
     }
   }
+
   def callToRed(wsClient: StandaloneWSClient): Future[Unit] = {
     val fromID = 1
     val response = wsClient.url(s"$host/get/$fromID").get()
     response flatMap { response =>
       wsClient.url(s"$host/go-to-red/$fromID").get().map { response2 =>
         val statusText: String = response.statusText
-        val body  = response.body[JsValue]
-        val body2  = (response2.body[JsValue] \ "color").as[String]
-//        val result = response.
-        println(s"Got a response $statusText:" +"\n"+ s"We are going from $body" +"\n"+ s"and stop at $body2")
-//        response.body[JsValue]
-
+        val body = response.body[JsValue]
+        val body2 = response2.body[JsValue]
+        println(s"Got a response $statusText:" + "\n" + s"We are going from $body" + "\n" + s"and stop at $body2")
       }
-
-    }
-//    val futureResult: Future[String] = wsClient.url(host).get().map { response =>
-//      (response.body \ "person" \ "name").as[String]
-//    }
-
-  }
-  def goToRed(wsClient: StandaloneWSClient): Future[Unit] = {
-
-    wsClient.url(s"$host/go-to-red/5").get().map { response =>
-      val statusText: String = response.statusText
-      val body = response.body[String]
-      println(s"Got a response $statusText: $body")
     }
   }
-//  def goToRedFor2(wsClient: StandaloneWSClient): Future[Unit] = {
+
+  def callToRedWithPath(wsClient: StandaloneWSClient): Future[Unit] = {
+    val fromID = 1
+    val response = wsClient.url(s"$host/get/$fromID").get()
+    response flatMap { response =>
+      wsClient.url(s"$host/go-to-red/$fromID").get().map { response2 =>
+        val statusText: String = response.statusText
+        val body = response.body[JsValue]
+        val body2 = response2.body[JsValue]
+        val stopID = (response2.body[JsValue] \ "id").as[Int]
+        println(s"Got a response $statusText:" + "\n" + s"We are going from $body" + "\n" + s"and stop at $body2")
+        println((fromID to stopID).toList)
+      }
+    }
+  }
+
+//  def callToRedTwoLights(wsClient: StandaloneWSClient): Future[Unit] = {
+//    val fromID1 = 1
+//    val fromID2 = 2
 //    val futureResponse: Future[StandaloneWSResponse] = for {
-//      responseOne <- wsClient.url(s"host/go-to-red/1").get()
-//      responseTwo <- wsClient.url(responseOne.body).get()
-//
-//    }
+//      responseOne <- wsClient.url(s"$host/go-to-red/$fromID1").get()
+//      responseTwo <- wsClient.url(s"$host/go-to-red/$fromID2").get()
+//    } yield (responseOne.body, responseTwo.body)
 //  }
+  //  def goToRedFor2(wsClient: StandaloneWSClient): Future[Unit] = {
+  //    val futureResponse: Future[StandaloneWSResponse] = for {
+  //
+  //
+  //    }
+  //  }
 }
 
